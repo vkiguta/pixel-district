8993eb2dc9324220ab10ac7e6d869bd7775aa617
Mwaniki Kiguta <vkiguta@yahoo.com>
2023-03-10 14:29:06 +0300

    name and icon gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Pixel District`,
+        short_name: `Pxl-Dist`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
